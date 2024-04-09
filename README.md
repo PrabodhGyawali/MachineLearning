3df3f42f9d4b4666b9980ab24601f8d7962d07c2
Prabodhcoolkid <prabodh.gyawali2005@gmail.com>
2024-04-09 04:44:36 +0100

    Scraper

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MachineLearning
 Repo with a bunch of ML projects that I am working with to get into the accelerating field of AI.
+
+I will be trying to get my own data through webscraping and writing scripts to fetch and execute API request.
+- I hope to learn more about Postman to write automated scripts
+- Learn more about the HTTP/HTTPS protocols and how signals are transferred across the web
---
+++ dataScraper.py
@@ -0,0 +1,41 @@
+import requests
+import json
+import threading
+import timeit
+
+URL = "https://www.the-numbers.com/movie/budgets/all"
+
+with open("request-data\\browserHeader.json", "r") as file:
+    header_string = file.read()
+
+header = json.loads(header_string)
+
+# Write file contents into a document
+def write_raw_data(http_response_text, file_name):
+    root_path = "raw_data/"
+    with open(root_path + file_name, "w") as file:
+        file.write(http_response_text)
+
+response = requests.get(url=URL, headers=header)
+
+# store response.text into a file
+write_raw_data(response.text, "Raw-Data-0.html")
+
+# Thread this task to make it really fast as we have to scrape 100+ pages
+def scraper():
+    URL_MORE = URL + f"/{i}01"
+    response = requests.get(url=URL_MORE, headers=header)
+
+    file_name = f"Raw-Data-{i}.html"
+    write_raw_data(response.text, file_name)
+
+for i in range(1, 65):
+    thread = threading.Thread(target=scraper)
+    thread.start()
+
+# Measuring time it takes for the code to execute
+timeit.timeit(scraper)
+
+
+
+
