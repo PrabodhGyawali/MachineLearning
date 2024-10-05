ebb3166a118a194f828df98d763f11a6ffe83432
Prabodhcoolkid <prabodh.gyawali2005@gmail.com>
2024-10-05 19:55:55 +0100

    data structures identified

--- MultiVariate_Regression_Project/scraper.py
+++ MultiVariate_Regression_Project/scraper.py
@@ -2,6 +2,7 @@ import requests
 import enum
 from bs4 import BeautifulSoup
 import threading
+import json
 
 PropertyType = enum.Enum(
     'DETACHED',
@@ -54,4 +55,71 @@ def calculate_distance_from(long, lat):
 
 filter = Filter(50)
 
-get_property_pages(filter)
+# get_property_pages(filter)
+
+def get_property_page_data():
+    """
+    Example of data structure:
+        {
+            'address': '2, Goff Street, Oxford, Oxfordshire OX2 8FW',
+            'propertyType': 'Terraced',
+            'bedrooms': 3,
+            'images': {
+                'imageUrl': '/spw/images/placeholder/no-image.svg',
+                'mediumImageUrl': '/spw/images/placeholder/no-image.svg',
+                'count': 0
+            },
+            'hasFloorPlan': False,
+            'transactions': [
+                {
+                    'displayPrice': '£875,000',
+                    'dateSold': '19 Jul 2024',
+                    'tenure': 'Freehold',
+                    'newBuild': False
+                },
+                {
+                    'displayPrice': '£899,950',
+                    'dateSold': '6 Oct 2021',
+                    'tenure': 'Freehold',
+                    'newBuild': True
+                }
+            ],
+            'location': {
+                'lat': 51.78529,
+                'lng': -1.29468
+            },
+            'detailUrl': 'https://www.rightmove.co.uk/house-prices/details/england-21208984?s=6b9864a6de50d4376a93898838919d075a782568fb9704afc19c852657763fc4'
+        }
+    """
+    with open('./html/page_1.html', 'r', encoding='utf-8') as file:
+        soup = BeautifulSoup(file, 'html.parser')
+
+        # Server side rendering so info is stored in script tags
+        scripts = soup.find_all('script')
+        # Filter out script tags with no attributes
+        scripts_no_attributes = [script for script in scripts if not script.attrs]
+        
+        # Get values inside script tags with housing info
+        preloaded_data = scripts_no_attributes[2].contents[0]
+        results = preloaded_data[29:]
+        results_json = json.loads(results)
+        print(results_json['results'])
+
+def get_property_page_details(file_path):
+    """
+        Get the div 'root' id and extract features: gardent, EPC rating, parking, etc.
+        Main features: number of bedrooms, bathrooms, property type, tenure, etc.
+    """
+    with open(file_path, 'r', encoding='utf-8') as file:
+        soup = BeautifulSoup(file, 'html.parser')
+
+        # Server side rendering so info is stored in script tags
+        scripts = soup.find_all('script')
+        # Filter out script tags with no attributes
+        scripts_no_attributes = [script for script in scripts if not script.attrs]
+        
+        # Get values inside script tags with housing info
+        preloaded_data = scripts_no_attributes[2].contents[0]
+        results = preloaded_data[29:]
+        results_json = json.loads(results)
+        print(results_json['results'])
